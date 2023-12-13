aab581bf8ee4d1573844f6d846ce1a6d1641fe16
MBar <thiswallz@gmail.com>
2023-12-13 18:35:46 -0300

    chore: database fundamentals, flyway and maven relocation

---
+++ tsg-backend/src/main/java/com/tsg/simulator/TsgApplication.java
@@ -0,0 +1,11 @@
+package com.tsg.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class TsgApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TsgApplication.class, args);
+	}
+}
